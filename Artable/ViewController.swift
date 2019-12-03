acbc828ed415b16839312ad239b394c4d9f05f05
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2019-12-03 13:00:45 +0200

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by adonis rumbwere on 3/12/2019.
+//  Copyright © 2019 Adonis Rumbwere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
