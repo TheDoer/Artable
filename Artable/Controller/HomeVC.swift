e1a64f2f6fd0b6787104e25583e31c6dc9f8fa05
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2019-12-31 14:21:44 +0200

    Setting up CollectionView and Model for Firestore

--- Artable/Controller/HomeVC.swift
+++ Artable/Controller/HomeVC.swift
@@ -11,32 +11,56 @@ import Firebase
 
 class HomeVC: UIViewController {
     
+    let backgroundImageView = UIImageView()
+    
     @IBOutlet weak var loginOutBtn: UIBarButtonItem!
     
-
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setBackground()
+        
+        
         
         //if the current user is not logged in, firebase with anonymously sign in
         if Auth.auth().currentUser == nil {
             Auth.auth().signInAnonymously { (result, error) in
                 
                 if let error = error {
-                    self.handleFireAuthError(error: error)
+                    Auth.auth().handleFireAuthError(error: error, vc: self)
                     debugPrint(error)
                 }
                 
             }
         }
         
+    
+        
     }
+    
+    func setBackground(){
+        view.addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = false
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = false
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = false
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = false
+        backgroundImageView.image = UIImage(named: "bg_cat3")
+        backgroundImageView.contentMode = .scaleAspectFill
+        view.sendSubviewToBack(backgroundImageView)
+        
+    }
+    
+    
     
     override func viewDidAppear(_ animated: Bool) {
         
         //We are logged in
         if let user = Auth.auth().currentUser, !user.isAnonymous {
-            loginOutBtn.title = "Adonis"
+            loginOutBtn.title = "Logout"
         } else {
             loginOutBtn.title = "Login"
         }
@@ -62,13 +86,13 @@ class HomeVC: UIViewController {
                 try Auth.auth().signOut()
                 Auth.auth().signInAnonymously { (result, error) in
                     if let error = error {
-                        self.handleFireAuthError(error: error)
+                        Auth.auth().handleFireAuthError(error: error, vc:self)
                         debugPrint(error)
                     }
                     self.presentLoginController()
                 }
             } catch  {
-                self.handleFireAuthError(error: error)
+                Auth.auth().handleFireAuthError(error: error, vc:self)
                 debugPrint(error)
             }
             
